daa3fee948a76013c6cb1b94ffaa8628d18152d5
Sang <tony_633@hotmail.com>
2017-09-01 19:30:13 -0400

    added react-sparklines

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+const API_KEY = '4bc522c0608824b007bab01a7b28635f';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+// request payload to reducers
+export function fetchWeather(city){
+	const url = `${ROOT_URL}&q=${city},ca&units=metric`;
+	const request = axios.get(url);
+	return {
+		type:FETCH_WEATHER,
+		payload:request
+	};
+}
---
+++ src/containers/WeatherList.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import Chart from '../components/Chart';
+
+
+class WeatherList extends Component {
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+		const lon = cityData.city.coord.lon;
+		const lat = cityData.city.coord.lat;
+
+		console.log(temps);
+
+		return (
+			<tr key={name}>
+				<td>{name}</td>
+				<td><Chart data={temps} color='blue' units="C" /></td>
+				<td><Chart data={pressures} color='red' units="hPa"/></td>
+				<td><Chart data={humidities} color='orange' units="%" /></td>
+			</tr>
+		);	
+	}
+
+
+	render(){
+		return(
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature (Celcius)</th>
+						<th>Pressure (hPa)</th>
+						<th>Humidity (%)</th>
+					</tr>
+				</thead>
+				<tbody>
+					{this.props.weather.map(this.renderWeather)}
+				</tbody>
+			</table>
+		);
+	}
+}
+
+function mapStateToProps(state){
+	return {
+		weather:state.weather
+	};
+}
+
+export default connect(mapStateToProps)(WeatherList);
