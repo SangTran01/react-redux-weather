df039779756096aa6f74f5765f2c87014cfa02bb
Sang <tony_633@hotmail.com>
2017-09-01 19:55:45 -0400

    fixed cors

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_KEY = '4bc522c0608824b007bab01a7b28635f';
-const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 // request payload to reducers
